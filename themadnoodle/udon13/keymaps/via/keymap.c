36e25303973b5577f923e7833231624ffa832e67
Jesse Leventhal <45154268+jessel92@users.noreply.github.com>
2024-02-02 16:00:14 -0500

    Added Custom Keycodes and Layer indicators

--- themadnoodle/udon13/keymaps/via/keymap.c
+++ themadnoodle/udon13/keymaps/via/keymap.c
@@ -3,6 +3,13 @@
 
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes {
+#ifdef VIA_ENABLE    // If you are using VIA, you can use the same keycodes as the default keymap
+    L_CYC = QK_KB_0  // Cycle through the layers
+#else                // If you are not using VIA, you can use your own keycodes
+    L_CYC = SAFE_RANGE
+#endif
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -50,3 +57,39 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
    
 };
 #endif
+
+//  ======================Custom Keycodes==========================
+
+// 1st layer on the cycle
+#define LAYER_CYCLE_START 0
+// Last layer on the cycle
+#define LAYER_CYCLE_END 3
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+
+        case L_CYC:
+            // Our logic will happen on presses, nothing is done on releases
+            if (!record->event.pressed) {
+                // We've already handled the keycode (doing nothing), let QMK know so no further code is run unnecessarily
+                return false;
+            }
+
+            uint8_t current_layer = get_highest_layer(layer_state);
+
+            // Check if we are within the range, if not quit
+            if (current_layer > LAYER_CYCLE_END || current_layer < LAYER_CYCLE_START) {
+                return false;
+            }
+
+            uint8_t next_layer = current_layer + 1;
+            if (next_layer > LAYER_CYCLE_END) {
+                next_layer = LAYER_CYCLE_START;
+            }
+            layer_move(next_layer);
+            return false;
+
+        default:
+            return true; // Process all other keycodes normally
+    }
+}
